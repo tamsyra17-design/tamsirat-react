59d64693aef3a65d3c88526804149653673c63f5
tamsyra17-design <tamsyra17@gmail.com>
2026-05-11 13:19:03 +0400

    Delete New Text Document.bat

--- New Text Document.bat
+++
@@ -1,7 +0,0 @@
-cd C:\Users\Dell\Desktop\tamsirat-react
-git init
-git add .
-git commit -m "Initial React website"
-git branch -M main
-git remote add origin https://github.com/tamsyra17-design/tamsirat-react.git
-git push -u origin main
